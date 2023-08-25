b70d9ed6a60a98b2984eb907b0212ccddffc1f6f
Archimedes Rocha <dev.archimedesrocha@gmail.com>
2023-08-25 13:36:51 -0300

    adapt code for running in heroku

--- task_api/src/index.ts
+++ task_api/src/index.ts
@@ -1,18 +1,23 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import * as express from "express"
-import * as bodyParser from "body-parser"
-import routes from "./routes"
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import routes from "./routes";
 
 const app = express();
+const PORT = process.env.PORT || 3333; // Usará a porta fornecida pelo Heroku, ou a porta 3333 se estiver executando localmente.
 
-createConnection().then(() => {
-  console.log("Connected to the database");
-}).catch(error => {
-  console.error("Error connecting to the database:", error);
+createConnection()
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
+
+app.use(bodyParser.json());
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(bodyParser.json())
-app.use(routes)
-
-app.listen(3333)
